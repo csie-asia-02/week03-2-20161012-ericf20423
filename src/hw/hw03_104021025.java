b9d2b9a76e5511e85a0eb2265d9f4cd0253f8561
ericf20423 <ericf20423@gmail.com>
2016-12-29 15:47:58 +0800

    104021125

---
+++ src/hw/hw03_104021025.java
@@ -0,0 +1,27 @@
+package hw;
+import java.util.*;
+public class hw03_104021025 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("輸入年分: ");
+		Scanner scn = new Scanner(System.in);
+		int year = scn.nextInt();
+		boolean flag = false;
+		if(year%4==0){
+			flag = true;
+			if (year%100==0){
+				flag = false;
+				if(year%400==0)
+					flag = true;
+			}
+		}
+		
+		if(flag)
+			System.out.println("閏年");
+		else
+			System.out.println("非閏年");
+
+	}
+
+}

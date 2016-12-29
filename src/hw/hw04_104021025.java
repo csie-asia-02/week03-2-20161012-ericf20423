1266b19bd712bb42190c6829d3c128d93faf37fc
ericf20423 <ericf20423@gmail.com>
2016-12-29 15:15:17 +0800

    104021125

--- src/hw/hw04_104021025.java
+++ src/hw/hw04_104021025.java
@@ -1,9 +1,16 @@
 package hw;
-
+import java.util.Scanner;
 public class hw04_104021025 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("輸入一整數n:");
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum =0;
+		for(int i = 1;i<=n;i++)
+			sum = i*(i+1)+sum;
+		System.out.println("sum = "+sum);
 
 	}
 

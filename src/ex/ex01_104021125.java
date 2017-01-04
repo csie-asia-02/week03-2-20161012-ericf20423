2b738b248f39f9cd0a65243bc408758a080c32f2
ericf20423 <ericf20423@gmail.com>
2017-01-04 11:28:39 +0800

    104021125

--- src/ex/ex01_104021125.java
+++ src/ex/ex01_104021125.java
@@ -1,15 +1,24 @@
 ﻿package ex;
 /*
  * Topic: 將一連串數字(每個數字為 0~9)輸入，計算其和。例如：輸入 12345，輸出為15 (1+2+3+4+5=15)
- * Date: 2016/10/12555
+ * Date: 2016/10/12
  * Author: 1040210XX 周永振老師
  */
 
-public class ex01_1040210XX {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import java.util.*;
+public class ex01_104021125 {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    int A=scn.nextInt();
+    int sum=0;
+    while(A>0){
+   
+    sum+=A%10;
+    A=A/10;
+    }
+System.out.println(sum);
+}
 }

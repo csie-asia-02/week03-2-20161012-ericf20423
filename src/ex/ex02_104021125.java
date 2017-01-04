d5411d9f4c163ec3da056bdbbcfe284ed0493b52
ericf20423 <ericf20423@gmail.com>
2017-01-04 11:29:33 +0800

    104021125

---
+++ src/ex/ex02_104021125.java
@@ -0,0 +1,16 @@
+package ex;
+import java.util.Scanner;
+public class ex02_103021125 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.print("輸入一個整數");
+		int a=scn.nextInt();
+		if(a%2==0){
+			System.out.println(a +"偶數");
+		}else{
+			System.out.println(a +"奇數");
+	}
+	}
+}
